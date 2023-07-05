b666f3bc9adf67fbc79c64296a6c23266becd8cf
Josue Sanchez Nima <josue.sanchez.n1402@hotmail.com>
2023-07-04 21:14:45 -0500

    Información de las cards

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -1,24 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/post.model';
 import { PostService } from 'src/app/services/post.service';
 
 @Component({
-  selector: 'bg-home',
+  selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  posts: any[] = [];
+  posts: Post[] = [];
 
   constructor(private postService: PostService) {}
 
   ngOnInit(): void {
-    this.postService.getPosts().subscribe(
-      (response) => {
-        this.posts = response;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
+    this.getPosts();
+  }
+
+  getPosts(): void {
+    this.postService.getPosts().subscribe((posts) => {
+      this.posts = posts.map((post) => {
+        const truncatedTitle = this.postService.truncateTitle(post.title);
+        const truncatedContent = this.postService.truncateContent(
+          post.content
+        );
+        return {
+          ...post,
+          title: truncatedTitle,
+          content: truncatedContent,
+        };
+      });
+    });
   }
 }
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Post } from '../models/post.model';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +11,23 @@ export class PostService {
 
   constructor(private http: HttpClient) {}
 
-  getPosts(): Observable<any> {
-    return this.http.get<any>(this.apiUrl);
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.apiUrl);
+  }
+
+  truncateTitle(title: string): string {
+    const maxLength = 25;
+    if (title.length > maxLength) {
+      return title.substr(0, maxLength) + '...';
+    }
+    return title;
+  }
+
+  truncateContent(content: string): string {
+    const maxLength = 176;
+    if (content.length > maxLength) {
+      return content.substr(0, maxLength) + '...';
+    }
+    return content;
   }
 }
